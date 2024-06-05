fa0a5d17b26ee89057e5be2c034db08ceceaf7a6
Zulvan Avito <shawnanggara@gmail.com>
2024-06-05 11:54:28 +0800

    Validate Form Signin Signup
    Validasi form SignIn dan SignUp nama, email, password

--- src/components/ui/fieldSignUp.jsx
+++ src/components/ui/fieldSignUp.jsx
@@ -1,22 +1,37 @@
-import React from "react";
+"use client";
+import { useForm } from "react-hook-form";
 import { Button } from "./button";
 import { Checkbox } from "./checkbox";
 
 const FieldSignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <form>
-      <div className="flex justify-center gap-4 justify-items-center mb-4">
-        <div className="mb-4 ">
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="flex justify-center gap-2 justify-items-center mb-4">
+        <div className="mb-4">
           <label className="block text-gray-700 mb-1" htmlFor="email">
             Name<sup className="text-red-500"> *</sup>
           </label>
           <input
-            className="shadow-md w-full px-3 py-2 border rounded-md"
+            className="shadow-md px-3 py-2 border rounded-md"
             type="name"
-            required
             id="name"
-            name="name"
+            {...register("name", { required: true })}
           />
+          <div>
+            {errors.name && (
+              <span className="text-red-500 text-xs">Nama harus di isi!</span>
+            )}
+          </div>
         </div>
         <div className="mb-4 ">
           <label className="block text-gray-700 mb-1" htmlFor="email">
@@ -25,10 +40,14 @@ const FieldSignUp = () => {
           <input
             className="shadow-md w-full px-3 py-2 border rounded-md"
             type="email"
-            required
             id="email"
-            name="email"
+            {...register("email", { required: true })}
           />
+          <div>
+            {errors.email && (
+              <span className="text-red-500 text-xs">Email harus di isi!</span>
+            )}
+          </div>
         </div>
       </div>
       <div className="mb-4">
@@ -38,22 +57,26 @@ const FieldSignUp = () => {
         <input
           className="shadow-md w-full px-3 py-2 border rounded-md"
           type="password"
-          required
           id="password"
-          name="password"
+          {...register("password", { required: true })}
         />
+        {errors.password && (
+          <span className="text-red-500 text-xs">Password harus di isi!</span>
+        )}
       </div>
-      <div className="flex items-center justify-start mt-5 mb-6">
+      <div className="flex items-center justify-start mt-5">
         <Checkbox />
         <label>
           <span className="ml-2 text-sm text-gray-700">
             I agree to terms & condition
           </span>
+          <div></div>
         </label>
       </div>
+
       <Button
         type="submit"
-        className="w-full shadow-lg text-white py-2 px-4 rounded-md hover:bg-primaryCustom-700"
+        className="w-full mt-6 shadow-lg text-white py-2 px-4 rounded-md hover:bg-primaryCustom-700"
       >
         Sign Up
       </Button>
