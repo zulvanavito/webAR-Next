106ad4fa0c4d49ecc1e9a8783aa97b2167754bce
Zulvan Avito <shawnanggara@gmail.com>
2024-07-12 18:53:07 +0800

    Sign In

--- src/components/ui/BackButton.jsx
+++ src/components/ui/BackButton.jsx
@@ -4,11 +4,11 @@ import { ArrowLeft } from "lucide-react";
 
 const BackButton = () => {
   return (
-    <>
+    <div>
       <Button className="bg-transparent border shadow-md rounded-full hover:bg-primaryCustom-100 max-w-fit">
         <ArrowLeft className="text-black justify-start"/>
       </Button>
-    </>
+    </div>
   );
 };
 
